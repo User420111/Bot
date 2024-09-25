ed0f2bcb6b9db57c460874411ff7023387fb5071
User420111 <amirziganshin20@gmail.com>
2024-09-25 13:25:22 +0300

    Add files via upload

---
+++ Amir/handlers/different_types.py
@@ -0,0 +1,13 @@
+from aiogram import Router, F
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(F.text)
+async def message_with_text(message: Message):
+    await message.answer("Это текстовое сообщение")
+
+@router.message(F.sticker)
+async def message_with_
+
+
