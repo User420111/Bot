412ab20d63b4f28c393b5860f76b2853a52d35b2
User420111 <amirziganshin20@gmail.com>
2024-11-06 13:12:15 +0300

    Add files via upload

---
+++ lexicon/lexicon.py
@@ -0,0 +1,13 @@
+LEXICON: dict[str, str] = {
+    '/start': 'Выберите пункт',
+    '/help': 'HELP',
+    'other': 'Введите /help, если не знаете, как пользоваться ботом',
+    'choice_website': "Выберите сайт",
+    'choice_task': "Выберите задание"
+
+}
+
+LEXICON_COMMANDS: dict[str, str] = {
+    '/start': "Запустить бота",
+    '/help': 'Справка по работе бота'
+}
